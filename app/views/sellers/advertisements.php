0fc2e4212a6d2cff7d55fa79b5164ae0b2d6eec6
Lahiru Kavishka <2020cs095@stu.ucsc.cmb.ac.lk>
2022-12-14 19:19:16 +0530

    linked new stylesheet

--- app/views/sellers/advertisements.php
+++ app/views/sellers/advertisements.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/form.css';?>">
     <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/sidebar.css';?>">
+    <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/profilebody.css';?>">
     <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/seller_advertisement.css';?>">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@500&display=swap" rel="stylesheet">
     <!-- <script src="https://kit.fontawesome.com/a076d05399.js" ></script> -->
@@ -43,40 +44,36 @@
     </nav>
     <div class="container">
         <div class="sidebar">
-                <a href="#"><i class="fas fa-qrcode"></i> <span>Dashboard</span></a>
-                <a href="#"> <i class="fa fa-cog" aria-hidden="true"></i><span>Profile Settings</span></a>
-                <a class="current" href="<?php echo URLROOT;?>/sellers/advertisements"> <i class="fa fa-ad" aria-hidden="true"></i><span>Advertisements</span></a>
-                <a href="<?php echo URLROOT;?>/sellers/advertise"> <i class="fa-solid fa-dollar-sign" aria-hidden="true"></i><span>Advertise</span></a>
-                <a href="#"> <i class="fa fa-comments"></i><span>Messages</span></a>       
+                <a class="current" href="<?php echo URLROOT;?>/sellers/advertisements"><i class="fas fa-qrcode"></i> <span>My Profile</span></a>
+                <a href="#"> <i class="fa fa-cog" aria-hidden="true"></i><span>Manage Users</span></a>
+                <a href="#"> <i class="fas fa-bookmark" aria-hidden="true"></i><span>Flags</span></a>
+                <a href="<?php echo URLROOT;?>/sellers/advertise"> <i class="fas fa-check-circle" aria-hidden="true"></i><span>Approvals</span></a>
+                <a href="#"> <i class="fas fa-child"></i><span>Help</span></a>       
         </div>
         <div class="poster_advertisements">
-            <h1>Posted Advertisements</h1>
-            <div class="header">
-                <div class="image">
-                    <img  class="two" src="" alt="photo">
-                </div>
-                <p class="one">Title</p>
-                <p class="two">Format</p>
-                <p class="three">Bids/Offers</p>
-                <p class="four">Price</p>
-                <p class="five">Edit</p>
-                <p class="six">Priview</p>
-
-            </div>
-            <?php foreach($data['advertisements'] as $advertisement): ?>
-            <div class="advertisements">
-                <div class="image">
-                    <img src="<?php echo URLROOT . '/img/Rectangle 100.png';?>" alt="photo">
-                </div>
-                <p class="one"><?php echo $advertisement->product_title;?></p>
-                <p class="two"><?php echo $advertisement->product_type;?></p>
-                <p class="three">N/A</p>
-                <p class="four">Rs.<?php echo $advertisement->price;?></p>
-                <a class="five" href="<?php echo URLROOT;?>/sellers/edit_advertisement/<?php echo $advertisement->product_id;?>">Edit</a>
-                <a class="six" href="<?php echo URLROOT;?>/sellers/advertisement/<?php echo $advertisement->product_id;?>">Preview</a>
-            </div>
-            <?php endforeach; ?>
+            <h1>Profile Details</h1>
+            
+            <div class="w3-container">
+      
+    <div class="rightbox">
+        <div class="profile">
+        
+          <h2>Full Name</h2>
+          <p>Hassnain Haider <button class="btn">update</button></p>
+          <h2>Birthday</h2>
+          <p>January 5</p>
+          <h2>Gender</h2>
+          <p>Male</p>
+          <h2>Email</h2>
+          <p>example@example.com <button class="btn">update</button></p>
+          <h2>Password</h2>
+          <p>******** <button class="btn">update</button></p>
         </div>
+      </div>
+    </div>
+
+            
+        
 
     </div>
 </body>
