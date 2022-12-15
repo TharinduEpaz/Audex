b110d60a0739f188aa22bff36bbe7ec53012b187
Lahiru Kavishka <2020cs095@stu.ucsc.cmb.ac.lk>
2022-12-15 23:48:55 +0530

    added changes

--- app/models/Admin.php
+++ app/models/Admin.php
@@ -19,6 +19,27 @@
             }
 
 
+            public function setDetails($arr)
+            {
+        
+                $this->db->query('INSERT INTO `user` (`user_id`, `email`, `first_name`, `address1`, `address2`, `phone_number`,) VALUES (NULL, 'lahiru', 'lahiru123@gmail.com', '0774809951', 'kirikiththa', '123456');');
+        
+                $this->db->bind(':id', $id);
+                $this->db->bind(':profession', $arr[0]);
+                $this->db->bind(':qualifications', $arr[1]);
+                $this->db->bind(':achievements', $arr[2]);
+                $this->db->bind(':description', $arr[3]);
+                $this->db->bind(':first_name', $arr[4]);
+                $this->db->bind(':second_name', $arr[5]);
+                $this->db->bind(':address1', $arr[6]);
+                $this->db->bind(':address2', $arr[7]);
+        
+                $this->db->execute();
+        
+                redirect('service_providers/profile');
+            }
+
+
 
     }
 
