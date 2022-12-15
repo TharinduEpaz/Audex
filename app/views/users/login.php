813f69f6d30a37837089a60b49edbd0fe2a35496
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2022-12-15 18:18:26 +0530

    login page bug fixed

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -19,7 +19,7 @@
         </label>
         <img src="<?php echo URLROOT . '/public/img/image 1.png';?>" alt="">
         <ul>
-            <li><a href="home.php" class="nav_tags">Home</a></li>
+            <li><a href="<?php echo URLROOT .'/service_providers/index'?>" class="nav_tags" class="nav_tags">Home</a></li>
             <li><a href="<?php echo URLROOT . '/buyers';?>" class="nav_tags">Shop</a></li>
             <li><a href="#" class="nav_tags">Sound Engineers</a></li>
             <li><a href="#" class="nav_tags">Events</a></li>
