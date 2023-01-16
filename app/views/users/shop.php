ce44c152b2c81656c52f25e7dda81d5900f67db3
Chamath Senanayake <2020cs171@stu.ucsc.cmb.ac.lk>
2023-01-16 14:05:07 +0530

    Did some css changes

--- app/views/users/shop.php
+++ app/views/users/shop.php
@@ -35,7 +35,11 @@
 
         <div class="container-data">
             <?php $i=0;
-            foreach($data['ads'] as $ads) :?>
+            foreach($data['ads'] as $ads) :
+                // if(!isset($data['ads'][$i])){
+                //     echo $i;
+                //    $i++;
+                // }?>
 
                 <div class="container-ad">
                     <div class="container-img">
@@ -43,9 +47,47 @@
                     </div>
                     <div class="title">
                         <h3><?php echo $ads->product_title ; ?></h3>
+                        <!-- <h3><?php echo $i ; ?></h3> -->
                         <?php 
-                            if($ads->product_type == 'auction'){
-                                echo '<h5>Auction</h5>';}?>
+                            if($ads->product_type == 'auction' ){
+                                echo '<h5>Auction</h5>';?>
+                                <!-- <?php echo $data['auction'][$i]->end_date;?> -->
+                                <!-- <div class="time">
+                                    <p>Time Left:&nbsp;</p>
+                                    <p id='remaining_time'></p>
+                                </div>
+                            <script>
+                                // Set the date we're counting down to
+                                var countDownDate = new Date("<?php echo $data['auction'][$i]->end_date;?>").getTime();
+
+                                // Update the count down every 1 second
+                                var x = setInterval(function() {
+                                
+                                  // Get today's date and time
+                                  var now = new Date().getTime();
+                                
+                                  // Find the distance between now and the count down date
+                                  var distance = countDownDate - now;
+                                
+                                  // Time calculations for days, hours, minutes and seconds
+                                  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                                  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                                  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                                  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                                
+                                  // Display the result in the element with id="demo"
+                                  document.getElementById("remaining_time").innerHTML = days + "d " + hours + "h "
+                                  + minutes + "m " + seconds + "s ";
+                                
+                                  // If the count down is finished, write some text
+                                  if (distance < 0) {
+                                      clearInterval(x);
+                                      document.getElementById("remaining_time").innerHTML = "EXPIRED";
+                                      window.location.href = "<?php echo URLROOT.'/users/bid_expired/'.$ads->product_id.'/'.$data['auction'][$i]->auction_id?>";
+                                  }
+                                }, 1000);
+                            </script> -->
+                            <?php } $i++;?>
                         <h4><?php echo 'RS:'. $ads->price ; ?></h4>
                         <a href="<?php if($ads->product_type == 'auction'){
                                 echo URLROOT . '/users/auction/'.$ads->product_id;
