a3ba6c5eeda3ed6dc08ec2b9497b512a951dc422
dineshWic <109039671+dineshWic@users.noreply.github.com>
2023-01-23 17:02:25 +0530

    finshed dislike functionality

--- app/config/config.php
+++ app/config/config.php
@@ -4,7 +4,7 @@
     define('DB_HOST', 'localhost');
     define('DB_USER','root');
     define('DB_PASS','');
-    define('DB_NAME','newuserdb');
+    define('DB_NAME','userdb');
 
     //App Root
     define('APPROOT', dirname(dirname(__FILE__)));
