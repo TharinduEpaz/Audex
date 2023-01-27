d273ef135b765bd7fec7eeb154516fad11d63043
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-01-27 14:26:10 +0530

    gg

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,9 @@
 <?php
     date_default_timezone_set("Asia/Kolkata");
     //DB params
-<<<<<<< HEAD
     define('DB_HOST', 'audex1.cqiwgljqq7rv.ap-northeast-1.rds.amazonaws.com');
     define('DB_USER','root');
     define('DB_PASS','Audex123');
-=======
-    define('DB_HOST', 'localhost');
-    //root.cqiwgljqq7rv.ap-northeast-1.rds.amazonaws.com
-    define('DB_USER','root');
-    define('DB_PASS','');
->>>>>>> a21bdf6458321d772cbb39761bdb4708ebeccc36
     define('DB_NAME','userdb');
 
     //App Root
