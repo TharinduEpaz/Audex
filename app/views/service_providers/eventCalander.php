64b37886397ff15bf11f03e512abcb4164a0bbc6
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-02-16 22:57:15 +0530

    calander basic UI done

--- app/views/service_providers/eventCalander.php
+++ app/views/service_providers/eventCalander.php
@@ -1,59 +1,126 @@
-
-
 <div class="service-provider-profile">
-<h2 style="margin-top:70px"> Your January Events will appear here </h2>
-  
-        <div class="button-section">
-            
-    
-        <div class="white-box">
-        <button>Next Month</button>
-        
+    <h2 style="margin-top:70px"> Event Calander </h2>
 
-        <div class="feed_items_container">
-            <div class="feed-item item1"></div>
-            <div class="feed-item item2"></div>
-            <div class="feed-item item3"></div>
-            <div class="feed-item item3"></div>
-            <div class="feed-item item1"></div>
-            <div class="feed-item item2"></div>
-            <div class="feed-item item3"></div>
-            <div class="feed-item item3"></div>
-           
-            
+    <div class="header-section">
+    <div class="previous-button">
+    <i class="fa fa-chevron-left" aria-hidden="true"></i></div>
 
-        </div>
- 
-        </div>
-
-        
-
+    <div class="month-display">
+        <span>January</span>
     </div>
+    <div class="next-button">
+        <i class="fa fa-chevron-right" aria-hidden="true"></i>
+    </div>
+</div>
 
-    <script src="<?php echo URLROOT . '/public/js/form.js';?>"></script>
+<div class="white-box">
+<div class="calendar">
 
-    <script>
+  <div class="calendar-days">
 
-        //keeping the sidebar button clicked at the page
+    <div class="calendar-date">1
+        <div class="calender-event">
+            <span>DJ fest FX</span>
+        </div>
+    </div>
+    <div class="calendar-date">2</div>
+    <div class="calendar-date">3</div>
+    <div class="calendar-date">4</div>
+    <div class="calendar-date">5</div>
+    <div class="calendar-date">6
+    <div class="calender-event">
+            <span>Millenium Night</span>
+        </div>
+        <div class="calender-event">
+            <span>DJ fest FX</span>
+        </div>
+    </div>
+    <div class="calendar-date">7</div>
+    <div class="calendar-date">8</div>
+    <div class="calendar-date">9</div>
+    <div class="calendar-date">10</div>
+    <div class="calendar-date">11
+    <div class="calender-event">
+            <span>Edu Session</span>
+        </div>
+    </div>
+    <div class="calendar-date">12</div>
+    <div class="calendar-date">13</div>
+    <div class="calendar-date">14</div>
+    <div class="calendar-date">15</div>
+    <div class="calendar-date">16</div>
+    <div class="calendar-date">17</div>
+    <div class="calendar-date">18
+    <div class="calender-event">
+            <span>DJ Night</span>
+        </div>
+        <div class="calender-event">
+            <span>PDR vibes</span>
+        </div>
+    </div>
+    <div class="calendar-date">19</div>
+    <div class="calendar-date">20</div>
+    <div class="calendar-date">21</div>
+    <div class="calendar-date">22
+    <div class="calender-event">
+            <span>DJ Night</span>
+        </div>
+        <div class="calender-event">
+            <span>Visiting Lecture</span>
+        </div>
+    </div>
+    <div class="calendar-date">23</div>
+    <div class="calendar-date">24</div>
+    <div class="calendar-date">25</div>
+    <div class="calendar-date">26</div>
+    <div class="calendar-date">27
+    <div class="calender-event">
+            <span>DJ fest FX</span>
+        </div>
+    </div>
+    <div class="calendar-date">28</div>
+    <div class="calendar-date">29</div>
+    <div class="calendar-date">30</div>
+    <div class="calendar-date">31
+    <div class="calender-event">
+            <span>DJ fest FX</span>
+        </div>
+    </div>
+  </div>
+</div>
 
-        link = document.querySelector('#calender');
-        link.style.background = "#E5E9F7";
-        link.style.color = "red";
 
-        error = document.querySelector('.red-alert');
-        error.style.color = "#FF0000"
 
-        editButton = document.querySelector('.btn');
 
-        if (error) {
+</div>
 
-            editButton.style.animation = "alert 2s ease 0s infinite normal forwards"
-            editButton.style.color = "#FF0000"
-            editButton.style.background = "#E5E9F7"
-            
-        }
 
-    </script>
+
+</div>
+
+<script src="<?php echo URLROOT . '/public/js/form.js'; ?>"></script>
+
+<script>
+    //keeping the sidebar button clicked at the page
+
+    link = document.querySelector('#calender');
+    link.style.background = "#E5E9F7";
+    link.style.color = "red";
+
+    error = document.querySelector('.red-alert');
+    error.style.color = "#FF0000"
+
+    editButton = document.querySelector('.btn');
+
+    if (error) {
+
+        editButton.style.animation = "alert 2s ease 0s infinite normal forwards"
+        editButton.style.color = "#FF0000"
+        editButton.style.background = "#E5E9F7"
+
+    }
+</script>
 
 </body>
+
 </html>
--- app/views/service_providers/header.php
+++ app/views/service_providers/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/sidebar.css?id=1425';?>">
-    <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/service_provider.css?id=325';?>">
+    <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/service_provider.css?id=25';?>">
     <link rel="stylesheet" href="<?php echo URLROOT . '/public/css/style.css?id=1445';?>">
   
 
