e493036af7fec7080b298a02044790885a58a4eb
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-05-06 23:16:08 +0530

    edit settings form validation done

--- app/controllers/Service_providers.php
+++ app/controllers/Service_providers.php
@@ -7,7 +7,7 @@ class Service_providers extends Controller
 
     public function __construct()
     {
-        if(isset($_SESSION['attempt'])){
+        if (isset($_SESSION['attempt'])) {
             unset($_SESSION['otp_email']);
             unset($_SESSION['phone']);
             unset($_SESSION['attempt']);
@@ -21,13 +21,13 @@ class Service_providers extends Controller
             redirect($_SESSION['user_type'] . 's/index');
         }
 
-        
+
         //Session timeout
-        if(isset($_SESSION['time'])){
-            if(time() - $_SESSION['time'] > 60*30){
+        if (isset($_SESSION['time'])) {
+            if (time() - $_SESSION['time'] > 60 * 30) {
                 // flash('session_expired', 'Your session has expired', 'alert alert-danger');
                 redirect('users/logout');
-            }else{
+            } else {
                 $_SESSION['time'] = time();
             }
         }
@@ -53,10 +53,10 @@ class Service_providers extends Controller
         ];
 
         $this->view('service_providers/profile', $data);
-
     }
-    public function setImage($temp_name, $file_name, $file_size, $file_error){
-    
+    public function setImage($temp_name, $file_name, $file_size, $file_error)
+    {
+
 
         // Work out the file extension
         $file_ext = explode('.', $file_name);
@@ -69,38 +69,37 @@ class Service_providers extends Controller
         if ($file_error === 0) {
             if (in_array($file_ext, $allowed)) {
                 if ($file_size <= 2097152) {
-                    
+
                     // $file_name_new = $_SESSION['user_id']. 'profile' . '.'  . $file_ext;
 
-                    $file_destination = dirname(APPROOT).'/public/uploads/profile/'.$file_name;
+                    $file_destination = dirname(APPROOT) . '/public/uploads/profile/' . $file_name;
 
                     // move_uploaded_file() is the built-in function in PHP that is used to move an uploaded file from its temporary location to a new location on the server
-                    
+
                     if (move_uploaded_file($temp_name, $file_destination)) {
 
                         $this->service_model->setImage($file_name, $_SESSION['user_id']);
-                    }
-                    else{
+                        return;
+                    } else {
                         echo 'error in  uploading';
                     }
-                }
-                else{
+                } else {
 
                     echo 'error large size';
                 }
-            }
-            else{
+            } else {
                 echo 'error not allowed this type';
             }
         }
-        redirect('service_providers/profile/');
+      
     }
-    public function settings()
+    public function settings($errors = [])
     {
         $details = $this->service_model->getDetails($_SESSION['user_id']);
 
         $data = [
-            'details' => $details
+            'details' => $details,
+            'errors' => $errors
         ];
         $this->view('service_providers/settings', $data);
     }
@@ -118,80 +117,124 @@ class Service_providers extends Controller
 
             if (isset($_POST['profession']) && $_POST['profession'] != '') {
 
-            $profession = $_POST['profession'];} 
-            else { $profession = $data['details']->profession;
+                $profession = $_POST['profession'];
+            } else {
+                $profession = $data['details']->profession;
             }
 
             if (isset($_POST['qualifications']) && $_POST['qualifications'] != '') {
 
                 $qualifications = $_POST['qualifications'];
-
             } else {
                 $qualifications = $data['details']->qualifications;
             }
             if (isset($_POST['achievements']) && $_POST['achievements'] != '') {
 
                 $achievements = $_POST['achievements'];
-
             } else {
                 $achievements = $data['details']->achievements;
             }
             if (isset($_POST['description']) && $_POST['description'] != '') {
 
                 $description = $_POST['description'];
-
             } else {
                 $description = $data['details']->description;
             }
             if (isset($_POST['first_name']) && $_POST['first_name'] != '') {
 
                 $first_name = $_POST['first_name'];
-
             } else {
                 $first_name = $data['details']->first_name;
             }
             if (isset($_POST['second_name']) && $_POST['second_name'] != '') {
 
                 $second_name = $_POST['second_name'];
-
             } else {
                 $second_name = $data['details']->second_name;
             }
             if (isset($_POST['address1']) && $_POST['address1'] != '') {
 
                 $address1 = $_POST['address1'];
-
             } else {
                 $address1 = $data['details']->address_line_one;
             }
             if (isset($_POST['address2']) && $_POST['address2'] != '') {
 
                 $address2 = $_POST['address2'];
-
             } else {
                 $address2 = $data['details']->address_line_two;
             }
+            if (isset($_POST['phone']) && $_POST['phone'] != '') {
 
-            if(!empty($_FILES['profile']['name'])){
-             
-                $this->feed();
+                $phone = $_POST['phone'];
+            } else {
+                $phone = $data['details']->phone;
+            }
+
+            if (!empty($_FILES['profile']['name'])) {
+
+
                 $temp_name = $_FILES['profile']['tmp_name'];
                 $file_name = $_FILES['profile']['name'];
                 $file_size = $_FILES['profile']['size'];
                 $file_error = $_FILES['profile']['error'];
 
-                $this->setImage($temp_name,$file_name,$file_size,$file_error);
+                $this->setImage($temp_name, $file_name, $file_size, $file_error);
             }
         }
 
-        $details = array($profession, $qualifications, $achievements, $description, $first_name, $second_name, $address1, $address2);
+        // $details = array($profession, $qualifications, $achievements, $description, $first_name, $second_name, $address1, $address2);
 
-        $this->service_model->setDetails($details, $_SESSION['user_id']);
+        $details = [
+            'profession' => $profession,
+            'qualifications' => $qualifications,
+            'achievements' => $achievements,
+            'description' => $description,
+            'first_name' => $first_name,
+            'second_name' => $second_name,
+            'address1' => $address1,
+            'address2' => $address2,
+            'phone' => $phone
+        ];
+
+        if(empty($this->validateProfileDetails($details))){
+            $this->service_model->setDetails($details, $_SESSION['user_id']);
+        }
+        else{
+            $errors = $this->validateProfileDetails($details);
+            $this->settings($errors);
+        }
 
         redirect('service_providers/profile/');
     }
 
-    
+    public function validateProfileDetails($details)
+    {
+        $errors = array();
+
+        $details = array_map('trim', $details);
+        $details = array_map('stripslashes', $details);
+        $details = array_map('htmlspecialchars', $details);
+
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $details['first_name'])) {
+            $nameErr = " First Name : Only letters and white space allowed";
+            array_push($errors, $nameErr);
+        }
+
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $details['second_name'])) {
+            $nameErr = "Second Name : Only letters and white space allowed";
+            array_push($errors, $nameErr);
+        }
+        if (!preg_match("/^[0-9]{10}$/", $details['phone'])) {
+            $phoneErr = "Phone number should be a 10-digit number";
+            array_push($errors, $phoneErr);
+        }
+
+        return $errors;
+    }
+ 
+
+
 
     public function feed()
     {
@@ -201,7 +244,7 @@ class Service_providers extends Controller
     public function addEvent()
     {
         $errors = [];
-        
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Validate form data
             $name = $_POST['eventname'];
@@ -212,7 +255,7 @@ class Service_providers extends Controller
             $description = $_POST['description'];
             $public = $_POST['event-type'];
             $time = $_POST['time'];
-        
+
             //uploading the image
 
             $temp_name = $_FILES['event-img']['tmp_name'];
@@ -230,85 +273,72 @@ class Service_providers extends Controller
             if ($file_error === 0) {
                 if (in_array($file_ext, $allowed)) {
                     if ($file_size <= 2097152) {
-                        
+
                         // $file_name_new = $_SESSION['user_id']. 'profile' . '.'  . $file_ext;
 
-                        $file_destination = dirname(APPROOT).'/public/uploads/events/'.$file_name;
-                        
+                        $file_destination = dirname(APPROOT) . '/public/uploads/events/' . $file_name;
+
                         // move_uploaded_file() is the built-in function in PHP that is used to move an uploaded file from its temporary location to a new location on the server
-                        
+
                         if (move_uploaded_file($temp_name, $file_destination)) {
 
                             $img = $file_name;
+                        } else {
 
-                        }
-                        else{
-                        
                             echo 'error in  uploading';
                         }
-                    }
-                    else{
+                    } else {
                         echo 'error large size';
                     }
-                }
-                else{
+                } else {
                     echo 'error not allowed this type';
                 }
             }
-        
-        // SEND THE DATA INTO THE DATABASE USING THE MODEL
 
-            $event_details = array($name, $date, $public, $location, $link, $description,$img,$time);
+            // SEND THE DATA INTO THE DATABASE USING THE MODEL
+
+            $event_details = array($name, $date, $public, $location, $link, $description, $img, $time);
 
             try {
                 $this->service_model->setEvent($event_details, $_SESSION['user_id']);
-               
-            } catch(PDOException $e) {
+            } catch (PDOException $e) {
                 echo "ERROR : " . $e->getMessage();
             }
-            
+        }
     }
-}
 
     public function dashboard()
     {
         $this->view('service_providers/dashboard');
     }
 
-    public function eventCalander(){
+    public function eventCalander()
+    {
 
         $month = $_GET['month'];
 
         if ($month == 'current') {
             $_SESSION['current'] = date('m');
             $_SESSION['current_y'] = date('y');
-        }
-        else if ($month == 'next' && $_SESSION['current'] < 12 ) {
+        } else if ($month == 'next' && $_SESSION['current'] < 12) {
             $_SESSION['current'] = $_SESSION['current'] + 1;
-           
-        }
-
-        else if($month == 'next' && $_SESSION['current'] >= 12){
+        } else if ($month == 'next' && $_SESSION['current'] >= 12) {
             $_SESSION['current_y'] = $_SESSION['current_y'] + 1;
             $_SESSION['current'] = 01;
+        } else if ($month == 'previous' && $_SESSION['current'] > 1) {
+            $_SESSION['current'] = $_SESSION['current'] - 1;
+        } else if ($month == 'previous' && $_SESSION['current'] <= 1) {
+            $_SESSION['current_y'] = $_SESSION['current_y'] - 1;
+            $_SESSION['current'] = 12;
         }
 
-        else if ($month == 'previous' && $_SESSION['current'] > 1) {
-            $_SESSION['current'] = $_SESSION['current'] - 1;
-        }
-    
-        else if ($month == 'previous' && $_SESSION['current'] <= 1){
-                $_SESSION['current_y'] = $_SESSION['current_y'] - 1;
-                $_SESSION['current'] = 12;
-            }
-        
-        
+
         // Update the displayed month name based on the new value of $current
         $year = $_SESSION['current_y'];
         $monthName = date('F', mktime(0, 0, 0, $_SESSION['current'], 1));
-        
-        $events = $this->service_model->getEventsByMonth($_SESSION['user_id'],$_SESSION['current']);
-        
+
+        $events = $this->service_model->getEventsByMonth($_SESSION['user_id'], $_SESSION['current']);
+
         $data = [
             'events' => $events,
             'month' => $monthName,
@@ -316,15 +346,17 @@ class Service_providers extends Controller
             'month_no' => $_SESSION['current'],
             'year_no' => $_SESSION['current_y']
         ];
-    
-        $this->view('service_providers/eventCalander',$data);
+
+        $this->view('service_providers/eventCalander', $data);
     }
 
-    public function messages(){
+    public function messages()
+    {
         $this->view('service_providers/chat');
     }
 
-    public function getEvent(){
+    public function getEvent()
+    {
         $id = $_GET['id'];
         $event = $this->service_model->getEventById($id);
         $name = $this->service_model->getEventOwner($id);
@@ -336,22 +368,19 @@ class Service_providers extends Controller
         return json_encode($data);
     }
 
-    public function addNewPost(){
+    public function addNewPost()
+    {
         $this->view('service_providers/addNewPost');
     }
 
-    public function editEvent(){
+    public function editEvent()
+    {
         $id = $_GET['id'];
         $event = $this->service_model->getEventById($id);
         $data = [
             'event' => $event,
-           
+
         ];
-        $this->view('service_providers/editEvent',$data);
-
+        $this->view('service_providers/editEvent', $data);
     }
-
 }
-
-
-?>
